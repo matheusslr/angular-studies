1544846c9fe690581c0dadf85505af4b08ad1a75
matheusslr <matheusslr@hotmail.com>
2023-12-07 08:44:53 -0300

    feat: working with interpolation

--- client-app/src/app/hello/hello.component.ts
+++ client-app/src/app/hello/hello.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrl: './hello.component.css'
 })
 export class HelloComponent {
+  firstName: string;
+  persons: Person[];
+  constructor() {
+    this.firstName = "firstName";
+    let example1 = new Person("lastName1", 23);
+    let example2 = new Person("lastName2", 30);
+  
+    this.persons = [example1, example2];
+  }
+}
 
+class Person {
+  constructor(
+    public lastName: string,
+    public age: number
+  ) { }
 }
