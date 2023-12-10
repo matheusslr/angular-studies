9c6f969e588134672798cf9ea3a2cb7f775ced2b
matheusslr <matheusslr@hotmail.com>
2023-12-09 23:22:06 -0300

    feat: routing clients-form

--- client-app/src/app/app.module.ts
+++ client-app/src/app/app.module.ts
@@ -5,16 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TemplateModule } from './template/template.module';
 import { HomeComponent } from './home/home.component';
+import { ClientsModule } from './clients/clients.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    TemplateModule
+    TemplateModule,
+    ClientsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client-app/src/app/clients/clients-form/clients-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clients-form',
+  templateUrl: './clients-form.component.html',
+  styleUrl: './clients-form.component.css'
+})
+export class ClientsFormComponent {
+
+}
