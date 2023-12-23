e0a384514190d41c7f29cd223886235678e1a034
matheusslr <matheusslr@hotmail.com>
2023-12-23 17:22:22 -0300

    refactor: Work entity to WorkProvided

--- clients-api/src/main/java/com/clientsapi/model/WorkProvided.java
+++ clients-api/src/main/java/com/clientsapi/model/WorkProvided.java
@@ -11,7 +11,7 @@ import java.math.BigDecimal;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Work {
+public class WorkProvided {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- clients-api/src/main/java/com/clientsapi/repository/WorkProvidedRepository.java
+++ clients-api/src/main/java/com/clientsapi/repository/WorkProvidedRepository.java
@@ -1,9 +1,9 @@
 package com.clientsapi.repository;
 
-import com.clientsapi.model.Work;
+import com.clientsapi.model.WorkProvided;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface WorkRepository extends JpaRepository<Work, Long> {
+public interface WorkProvidedRepository extends JpaRepository<WorkProvided, Long> {
 }
