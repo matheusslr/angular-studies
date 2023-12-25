b2c0e4a5f8aa3ec8ce368b561329066ab66966b1
matheusslr <matheusslr@hotmail.com>
2023-12-25 11:12:10 -0300

    feat: validation fields in workProvidedController endpoints

--- clients-api/src/main/java/com/clientsapi/controller/v1/WorkProvidedController.java
+++ clients-api/src/main/java/com/clientsapi/controller/v1/WorkProvidedController.java
@@ -3,6 +3,7 @@ package com.clientsapi.controller.v1;
 import com.clientsapi.dto.WorkProvidedDTO;
 import com.clientsapi.model.WorkProvided;
 import com.clientsapi.service.v1.WorkProvidedService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,15 +18,15 @@ public class WorkProvidedController {
     private WorkProvidedService service;
 
     @PostMapping
-    public ResponseEntity<WorkProvided> save(@RequestBody WorkProvidedDTO dto){
+    public ResponseEntity<WorkProvided> save(@Valid @RequestBody WorkProvidedDTO dto) {
         return new ResponseEntity<>(service.save(dto), HttpStatus.CREATED);
     }
 
     @GetMapping
     public ResponseEntity<List<WorkProvided>> findByClientNameAndMonth(
             @RequestParam(value = "name", required = false, defaultValue = "") String name,
-            @RequestParam(value = "month", required = false) Integer month
-    ){
+            @RequestParam(value = "month") Integer month
+    ) {
         return ResponseEntity.ok(service.findByClientNameAndMonth(name, month));
     }
 }
--- clients-api/src/main/java/com/clientsapi/dto/WorkProvidedDTO.java
+++ clients-api/src/main/java/com/clientsapi/dto/WorkProvidedDTO.java
@@ -1,6 +1,8 @@
 package com.clientsapi.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,9 +16,13 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 public class WorkProvidedDTO {
+    @NotEmpty(message = "{field.description.required}")
     private String description;
+    @NotNull(message = "{field.client.required}")
     private Long idClient;
+    @NotNull(message = "{field.price.required}")
     private BigDecimal price;
+    @NotNull(message = "{field.date.required}")
     @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate date;
 }
