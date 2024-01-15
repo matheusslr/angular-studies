08b6e1a3b449bf595c52cb7434cd725dec9be38e
matheusslr <matheusslr@hotmail.com>
2024-01-15 08:53:53 -0300

    feat: adding default route to login

--- client-app/src/app/app-routing.module.ts
+++ client-app/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   {
